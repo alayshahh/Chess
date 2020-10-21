8eadea8f9945d292c810bbaa603eb4ee18638cad
Anshika Khare <51126871+anshikakhare@users.noreply.github.com>
2020-10-20 21:04:27 -0400

    bishop rook queen

--- src/chess/Bishop.java
+++ src/chess/Bishop.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,7 +27,43 @@ public class Bishop extends Piece {
 	@Override
 	public List<Move> allPossibleMoves(Board gameBoard) {
 		// TODO Auto-generated method stub
-		return null;
+		List<Move> moves = new ArrayList<>();
+		Piece [] [] board = gameBoard.board;
+		Team opponent;
+		if(team == Team.WHITE) {
+			opponent = Team.BLACK;
+		}else opponent = Team.WHITE;
+		int rank = getRank();
+		int file = getFile();
+		boolean up = rank+1<8;
+		boolean down = rank-1 > -1;
+		boolean left = file -1> -1;
+		boolean right = file+1< 8;
+		if (up) {
+			if(left) {
+				if(board[rank+1][file-1]==null||board[rank+1][file-1].team==opponent) { //upright
+					moves.add(new Move(curLoc, new Location(rank+1, file-1)));
+				}
+			}
+			if(right) {
+				if(board[rank+1][file+1]==null||board[rank+1][file+1].team==opponent) { //upleft
+					moves.add(new Move(curLoc, new Location(rank+1, file+1)));
+				}
+			}
+		}
+		if (down) {
+			if(left) {
+				if(board[rank-1][file-1]==null||board[rank-1][file-1].team==opponent) { //down right
+					moves.add(new Move(curLoc, new Location(rank-1, file-1)));
+				}
+			}
+			if(right) {
+				if(board[rank-1][file+1]==null||board[rank-1][file+1].team==opponent) { //down left
+					moves.add(new Move(curLoc, new Location(rank-1, file+1)));
+				}
+			}
+		}
+		return moves;
 	}
 
 }

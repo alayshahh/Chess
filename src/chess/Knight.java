9c3e5a03b84cadee3f53e8b4cee73c8459fa95f9
Alay Shah <alayshah9@icloud.com>
2020-10-22 12:40:43 -0400

    fixed the Knight Move

--- src/chess/Knight.java
+++ src/chess/Knight.java
@@ -24,68 +24,19 @@ public class Knight extends Piece {
 	public List<Move> allPossibleMoves(Board gameBoard) {
 		// TODO Auto-generated method stub
 		List<Move> moves = new ArrayList<>();
+//		return moves;
 		Piece [] [] board = gameBoard.board;
 		Team opponent;
 		if(team == Team.WHITE) {
 			opponent = Team.BLACK;
 		}else opponent = Team.WHITE;
-		int rank = getRank();
-		int file = getFile();
-		boolean up = rank+1<8;
-		boolean down = rank-1 > -1;
-		boolean twoLeft = file -3> -1;
-		boolean twoRight = file+3< 8;
-		
-		boolean twoUp = rank+3<8;
-		boolean twoDown = rank-3 > -1;
-		boolean left = file -1> -1;
-		boolean right = file+1< 8;
-		if (up) {
-			if(twoLeft) {
-				if(board[rank+1][file-3]==null||board[rank+1][file-1].team==opponent) { //up left
-					moves.add(new Move(curLoc, new Location(rank+1, file-1)));
-				}
-			}
-			if(twoRight) {
-				if(board[rank+1][file+3]==null||board[rank+1][file+1].team==opponent) { //up right
-					moves.add(new Move(curLoc, new Location(rank+1, file+1)));
-				}
-			}
-		}
-		if (down) {
-			if(twoLeft) {
-				if(board[rank-1][file-3]==null||board[rank-1][file-1].team==opponent) { //down left
-					moves.add(new Move(curLoc, new Location(rank-1, file-1)));
-				}
-			}
-			if(twoRight) {
-				if(board[rank-1][file+3]==null||board[rank-1][file+1].team==opponent) { //down right
-					moves.add(new Move(curLoc, new Location(rank-1, file+1)));
-				}
-			}
-		}
-		
-		if (twoUp) {
-			if(left) {
-				if(board[rank+2][file-1]==null||board[rank+1][file-1].team==opponent) { //up left
-					moves.add(new Move(curLoc, new Location(rank+1, file-1)));
-				}
-			}
-			if(right) {
-				if(board[rank+2][file+1]==null||board[rank+1][file+1].team==opponent) { //up right
-					moves.add(new Move(curLoc, new Location(rank+1, file+1)));
-				}
-			}
-		}
-		if (twoDown) {
-			if(left) {
-				if(board[rank-2][file-1]==null||board[rank-1][file-1].team==opponent) { //down left
-					moves.add(new Move(curLoc, new Location(rank-1, file-1)));
-				}
-			}
-			if(right) {
-				if(board[rank-2][file+1]==null||board[rank-1][file+1].team==opponent) { //down right
-					moves.add(new Move(curLoc, new Location(rank-1, file+1)));
+		int rnk = getRank();
+		int fle = getFile();
+		int [] [] knightPos = {{rnk+1, fle+2},{rnk+2,fle+1},{rnk-2, fle+1}, {rnk-1, fle+2},{rnk-2, fle-1}, {rnk-1,fle-2}, {rnk+1,fle-2}, {rnk+2,fle-1}};
+		for( int [] loc: knightPos) {
+			if(loc[0]>-1 && loc[0]<8 && loc[1]>-1 && loc[1]<8) {
+				if(board[loc[0]][loc[1]]==null || board[loc[0]][loc[1]].team==opponent) {
+					moves.add(new Move(curLoc, new Location(loc[0], loc[1])));
 				}
 			}
 		}
